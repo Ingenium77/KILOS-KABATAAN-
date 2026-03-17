42b61fae9f6bb7a16e81b9cd0bb345ec3929cd3b
KuuShyn <49732964+KuuShyn@users.noreply.github.com>
2026-03-18 05:45:52 +0800

    Loading Screen by the "Expert"

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,20 +1,15 @@
-extends Node2D
+extends Control
 
 
 @export var initial_scene: StringName = &""
 @export var play_button: Button
 
-func _ready() -> void:
-	play_button.pressed.connect(_on_button_pressed)
-
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://SETTINGS.tscn")
 
-
 func _on_play_pressed() -> void:
-	print("PLAY pressed)")
-
-
+	SceneLoader.load_scene(initial_scene)
+	
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://CREDITS.tscn")
