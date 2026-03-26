a41829edfeb56f035a1b81997058590be8fdde1c
KuuShyn <49732964+KuuShyn@users.noreply.github.com>
2026-03-27 07:52:49 +0800

    something changes

--- scripts/something.gd
+++ scripts/something.gd
@@ -4,13 +4,5 @@ func remove_something() -> void:
 	print("Trying to finish quest: remove_something")
 	print("Active quests: ", QuestManager.active_quests.keys())
 	print("Is it active? ", QuestManager.active_quests.has("remove_something"))
-	#QuestManager.reach_goal("remove_something")
-	#await get_tree().create_timer(2.0).timeout
-	QuestManager.finish_quest("remove_something", 2)
+	QuestManager.finish_quest("remove_something", 1)
 	queue_free()
-
-func check_list_something() -> void:
-	QuestManager.reach_goal("check_list")
-	await get_tree().create_timer(3.0).timeout
-	QuestManager.finish_quest("check_list",2)
-	QuestManager.start_quest("back_to_house")
