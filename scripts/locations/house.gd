c267631023e722aeee6b5a4ba384e3131568e57b
Ingenium77 <castillolouelleaiden@gmail.com>
2026-04-05 11:52:39 +0800

    game all done

--- scripts/locations/house.gd
+++ scripts/locations/house.gd
@@ -4,9 +4,24 @@ var entered = false
 
 var something_scene = preload("res://scenes/Locations/something.tscn")
 
+@onready var restart_button = $RestartButton
+@onready var restart_art = $RestartButton/RestartArt
+
 func _ready() -> void:
 	QuestManager.start_quest("talk_to_tatay")
+	restart_button.visible = false  # hide by default
+	restart_art.visible = false
 
+	# check if ending already reached
+	check_restart_button()
+
+func _process(delta):
+	check_restart_button()
+
+func check_restart_button():
+	if QuestManager.is_quest_completed("break_the_news_to_tatay"):
+		restart_button.visible = true
+		restart_art.visible = true
 	
 #	after doing something in brgy hall either workssssss
 	if QuestManager.is_quest_active("back_to_house"):
@@ -16,3 +31,9 @@ func _ready() -> void:
 
 func _on_settings_pressed() -> void:
 	SceneLoader.load_scene("res://scenes/Menu/SETTINGS.tscn")
+
+
+func _on_restart_button_pressed() -> void:
+	QuestManager.active_quests.clear()
+	QuestManager.completed_quests.clear()
+	SceneLoader.load_scene("res://scenes/Menu/MAIN MENU.tscn")
